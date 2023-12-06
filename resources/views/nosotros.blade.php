aa738f779acc1fde63940fa85fdc373536f89695
Vanesa Villafana Liñan <denisevillafana@hotmail.com>
2023-12-05 22:58:44 -0300

    upload-2

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -4,8 +4,11 @@
 <div class="container mt-4">
 
     <div class="jumbotron text-center">
-        <h2>Sobre TUFI Store</h2>
-        <p>Breve introducción acerca de la empresa...</p>
+        <h2>Bienvenidos a Tufi Store: El Paraíso de los Coleccionistas de Figuritas</h2>
+        <p>En Tufi Store, entendemos la emoción y la dedicación que conlleva la tarea de completar un álbum de figuritas. Somos más que una tienda online; somos un punto de encuentro para coleccionistas apasionados, expertos y novatos, que buscan esa pieza especial para dar vida a sus colecciones.</p>
+        <p>Nuestra amplia selección de figuritas sueltas abarca una variedad de temáticas y épocas, asegurando que cada coleccionista encuentre exactamente lo que busca. Desde las clásicas hasta las más raras, cada figurita en nuestro catálogo es una oportunidad para acercarte a completar tu álbum deseado.</p>
+        <p>Celebramos cada historia detrás de las colecciones y fomentamos la conexión entre aficionados de todas partes del mundo.</p>
+        <p>En Tufi Store, cada clic te acerca a completar tu sueño. Explora nuestra colección hoy y ¡descubre el mundo fascinante de las figuritas!</p>
     </div>
 
     <div class="row text-center">
@@ -14,7 +17,7 @@
                 <img src="ruta/a/imagen/mision.jpg" class="card-img-top" alt="">
                 <div class="card-body">
                     <h3 class="card-title">Misión</h3>
-                    <p>Descripción breve de la misión de la empresa.</p>
+                    <p>En Tufi Store, nuestra misión es conectar a coleccionistas apasionados con las figuritas que necesitan para completar sus álbumes. Nos esforzamos por facilitar una experiencia de compra accesible, confiable y personalizada, garantizando que cada cliente encuentre esa pieza especial que busca, sin importar cuán rara sea.</p>
                 </div>
             </div>
         </div>
@@ -24,7 +27,7 @@
                 <img src="ruta/a/imagen/vision.jpg" class="card-img-top" alt="">
                 <div class="card-body">
                     <h3 class="card-title">Visión</h3>
-                    <p>Descripción breve de la visión de la empresa.</p>
+                    <p>Nuestra visión es ser la principal referencia en el mercado de figuritas sueltas, creando una comunidad global donde coleccionistas de todas las edades puedan encontrar y celebrar su pasión. Aspiramos a transformar Tufi Store en un espacio que no solo complete colecciones, sino que también fomente recuerdos duraderos.</p>
                 </div>
             </div>
         </div>
@@ -34,7 +37,13 @@
                 <img src="ruta/a/imagen/valores.jpg" class="card-img-top" alt="">
                 <div class="card-body">
                     <h3 class="card-title">Valores</h3>
-                    <p>Descripción breve de los valores de la empresa.</p>
+                    <ul>
+                        <li><strong>Pasión por el coleccionismo:</strong> Comprometidos con la emoción y la dedicación que implica completar una colección.</li>
+                        <li><strong>Confiabilidad:</strong> Ofrecemos un servicio seguro y transparente donde los coleccionistas pueden confiar.</li>
+                        <li><strong>Inclusividad:</strong> Reconocemos y valoramos la diversidad de nuestros clientes, desde los más jóvenes hasta los más experimentados.</li>
+                        <li><strong>Innovación:</strong> Continuamente buscamos formas de mejorar y simplificar el proceso de búsqueda y adquisición de figuritas.</li>
+                        <li><strong>Comunidad:</strong> Fomentamos un sentido de pertenencia y conexión entre coleccionistas de todo el mundo.</li>
+                    </ul>
                 </div>
             </div>
         </div>
